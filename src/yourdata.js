c1a2f279b261cbf7cf6bcd7ec8dd29caec2eff66
Than Htike Soe <than@ThannoMacBook-puro.local>
2022-10-29 17:24:01 +0800

    footer

--- src/yourdata.js
+++ src/yourdata.js
@@ -180,7 +180,7 @@ experienceResponsibilityli1_4:
 
   //   Contact Section --------------
 
-  contactSubHeading: "Let's create your next experience together",
+  contactSubHeading: "Just say hi and add me to your contacts?Always welcome your message ^^",
   social: [
     // Add Or Remove The Link Accordingly
     { img: githubIcon, url: "https://github.com/ThanHtikeJack" },
